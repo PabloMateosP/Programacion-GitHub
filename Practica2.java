4f46641f7823859650c06fa5d361c7c48d0f5e9f
PabloMateosP <pmatpal0105@g.educaand.es>
2022-11-13 01:33:01 +0100

    Practica 2
    Comienzo de la practica

--- Practica2.java
+++ Practica2.java
@@ -1,13 +1,35 @@
 public class Practica2 {
     public static void main(String[] args) {
-        int max = 100;
-        int[] randomNumber = {(int)Math.floor(Math.random()*max)};
-        int[] primo = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89 ,97};
-        
-        if (randomNumber == primo)
-            System.out.println("El número " + randomNumber + " no es un número primo ");
-            
-        else 
-            System.out.println("El número " + randomNumber + " es un número primo ");  
+        int max = 100; 
+        int randomNumber = (int)Math.floor(Math.random()*max); 
+        if (randomNumber <= 1)
+            System.out.println("El número " + randomNumber + " no es primo " );
+        else if (randomNumber == 2)
+            System.out.println("El número " + randomNumber + " es un número primo ");
+        else {
+            for (int i=2; 2<randomNumber; i++){
+               if (randomNumber % i == 0 )
+                   System.out.println("El número " + randomNumber + " no es un número primo ");
+               } 
+          System.out.println("El número " + randomNumber + " es un número primo "); 
+       }
     }
+    
+//    public static boolean primo(int n) {
+//        if (n <= 1) 
+//            return false;
+//        else if (n==2)
+//          return true;
+//        else {
+//           for (int i = 2; i<n; i++){
+//                if (n % i == 0)
+//                    return false;
+//            }
+//            return true;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 100; i++)
+//            System.out.println(i + " es " + primo(i)); 
+//    }
 }
